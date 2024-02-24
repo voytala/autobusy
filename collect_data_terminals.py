9824244357ef7b1b7898a0e346bad47073310b8f
voytala <95651945+voytala@users.noreply.github.com>
2024-02-24 14:02:28 +0100

    Delete collect_data_terminals.py

--- collect_data_terminals.py
+++
@@ -1,8 +0,0 @@
-import time
-import requests
-from json_utilities import get_json, save_json, APIKEY, URL_SCHEDULES
-from datetime import datetime
-    
-fname = f"schedules.json"
-schedules = get_json(URL_SCHEDULES)
-save_json(schedules, fname)
