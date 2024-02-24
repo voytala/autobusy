2ed9dbcadc38dd189c00d2da3641e78bd499db1c
voytala <95651945+voytala@users.noreply.github.com>
2024-02-24 14:01:03 +0100

    Delete wizualizacja directory

--- wizualizacja/csv_data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import geopandas as gpd
-
-def load_data(filename):
-    return pd.read_csv(filename, sep=';')
-
-def load_boundary(filename):
-    return gpd.read_file(filename)
--- wizualizacja/map_config.py
+++
@@ -1,4 +0,0 @@
-warsaw_lon_min = 20.40
-warsaw_lon_max = 21.60
-warsaw_lat_min = 51.90
-warsaw_lat_max = 52.60
